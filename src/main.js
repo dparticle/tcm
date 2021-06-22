f79b2566ded7a860245f68f6b4ca07945fe41980
dparticle <dparticle@163.com>
2021-06-22 10:30:38 +0800

    feat: 图片懒加载

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import Vant from "vant";
+import { Lazyload } from "vant";
 import "vant/lib/index.css";
 import store from "./store";
 import api from "./api";
 
 Vue.config.productionTip = false;
 Vue.use(Vant);
+Vue.use(Lazyload);
 Vue.prototype.$api = api;
 
 new Vue({
